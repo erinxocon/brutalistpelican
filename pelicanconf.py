a3fff3fbc140d35f878757b0f70ffb821dbb64a7
Erin O'Connell <erin@occipital.com>
2019-01-29 23:35:47 -0700

    init commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Matt McManus'
-SITENAME = 'Brutalist Pelican Theme'
-SITEURL = ''
+AUTHOR = "Erin O'Connell"
+SITENAME = 'The Trans Pythonista'
+SITEURL = '/'
 
 # Categories
 USE_FOLDER_AS_CATEGORY = True
@@ -50,37 +50,33 @@ THEME = 'themes/brutalist'
 ## used for OG tags and Twitter Card data on index page
 SITEIMAGE = 'site-cover.jpg'
 ## used for OG tags and Twitter Card data of index page
-SITEDESCRIPTION = 'A simple, accessible, content-first Pelican theme inspired by David Bryant Copeland\'s https://brutalist-web.design/'
+SITEDESCRIPTION = 'The ramblings of a transgender python programmer.'
 ## path to favicon
-FAVICON = 'pelly.png'
+FAVICON = 'icon.png'
 ## path to logo for nav menu (optional)
-LOGO = 'pelly.png'
+LOGO = 'icon.png'
 ## first name for nav menu if logo isn't provided
-FIRST_NAME = 'Brutalist'
+FIRST_NAME = 'Erin'
 ## google analytics (fake code commented out)
 # GOOGLE_ANALYTICS = 'UA-0011001-1'
 ## Twitter username for Twitter Card data
-TWITTER_USERNAME = '@mcman_s'
+TWITTER_USERNAME = '@erinxocon'
 ## Toggle display of theme attribution in the footer (scroll down and see)
 ## Attribution is appreciated but totally fine to turn off!
-ATTRIBUTION = True
+ATTRIBUTION = False
 ## Add a link to the tags page to the menu
 ## Other links can be added following the same tuple pattern 
 MENUITEMS = [('tags', '/tags')]
 ## Social icons for footer
 ## Set these to whatever your unique public URL is for that platform
 ## I've left mine here as a example
-STRAVA = 'https://www.strava.com/athletes/27234301'
-TWITTER = 'https://twitter.com/mcman_s'
-INSTAGRAM = 'https://instagram.com/mcman_s'
-GITHUB = 'https://github.com/mamcmanus'
-TELEGRAM = 'https://t.me/mcman_s'
-GOODREADS = 'https://www.goodreads.com/user/show/48849158-matthew-mcmanus'
-FOURSQUARE = 'https://foursquare.com/mcman_s'
-UNTAPPD = 'https://untappd.com/user/mcman_s'
+TWITTER = 'https://twitter.com/erinxocon'
+INSTAGRAM = 'https://instagram.com/erinxocon'
+GITHUB = 'https://github.com/erinxocon'
+
 ## Disqus Sitename for comments on posts
 ## Commenting mine out for this theme site
-DISQUS_SITENAME = 'brutalistpelican'
+#DISQUS_SITENAME = 'brutalistpelican'
 ## Gravatar
 ## Commenting mine out so you can see how the theme looks without one
 ## See https://mamcmanus.com to see what it looks like with a Gravatar
@@ -89,7 +85,7 @@ DISQUS_SITENAME = 'brutalistpelican'
 
 # PLUGINS
 PLUGIN_PATHS = ['plugins']
-PLUGINS = ['sitemap', 'category_order', 'w3c_validate', 'optimize_images', 'gzip_cache']
+PLUGINS = ['sitemap', 'w3c_validate', 'gzip_cache']
 
 ## SITEMAP PLUGIN
 SITEMAP = {
@@ -106,7 +102,6 @@ SITEMAP = {
     },
 }
 
-## Github URL for fork ribbon
-## Only used on theme site, not personal blog
-GITHUB_URL = 'https://github.com/mamcmanus/brutalistpelican'
-GITHUB_POSITION = 'left'
+TYPOGRIFY = True
+FEED_ATOM = None
+FEED_RSS = None
